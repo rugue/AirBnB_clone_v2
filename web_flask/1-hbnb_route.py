d9029fb924cda5937f00ec43e931972c5f3df326
Osarugue Enehizena <nehirugue@gmail.com>
2024-03-25 15:39:17 -0700

    done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ Starts a Flash Web Application HBNB"""
 from flask import Flask
 app = Flask(__name__)
@@ -17,4 +18,3 @@ def hbnb():
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
-
